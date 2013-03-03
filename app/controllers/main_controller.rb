ea7fae62346e5c8e150907c0f1e6776c993aa3ab
Dirk Milotz <dmilotz@gmail.com>
2013-03-03 10:16:38 -0800

    hii

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,11 @@
 class MainController < ApplicationController
   def index
+        @topics = Article.select(:topic)
+
+  end
+
+  def get_urls
+    selected_topic = Article.find(params[:article][:topic])
+    @urls = Article.where("topic = ?", selected_topic)
   end
 end
